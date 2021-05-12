f8b3fc8f3613a18e3f9c07b18bf742382a032734
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-05-12 18:55:11 -0300

    implementação do menu responsivo 2.0 e mudança do curriclo

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,28 @@
 const hamburger = document.getElementById("hamburger");
-const navUL = document.getElementById("nav-ul");
+const menu = document.getElementById("menu");
+
+const home = document.getElementById("clickHome");
+const about = document.getElementById("clickAbout");
+const project = document.getElementById("clickProject");
+const contact = document.getElementById("clickContact");
+
 
 hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
+    menu.classList.toggle('show');
+})
+
+home.addEventListener('click', () => {
+    menu.classList.toggle('show');
+})
+
+about.addEventListener('click', () => {
+    menu.classList.toggle('show');
+})
+
+project.addEventListener('click', () => {
+    menu.classList.toggle('show');
+})
+
+contact.addEventListener('click', () => {
+    menu.classList.toggle('show');
 })
