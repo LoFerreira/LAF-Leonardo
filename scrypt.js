782a3e5703feb07eb9633678458610494b023da3
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-05-13 19:56:16 -0300

    mudança visual do site

--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,6 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
+const navMenu = document.getElementById("nav-menu");
 
 const home = document.getElementById("clickHome");
 const about = document.getElementById("clickAbout");
@@ -7,22 +8,28 @@ const project = document.getElementById("clickProject");
 const contact = document.getElementById("clickContact");
 
 
+
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
+    navMenu.classList.toggle('leleo');
 })
 
 home.addEventListener('click', () => {
     menu.classList.toggle('show');
+    navMenu.classList.toggle('leleo');
 })
 
 about.addEventListener('click', () => {
     menu.classList.toggle('show');
+    navMenu.classList.toggle('leleo');
 })
 
 project.addEventListener('click', () => {
     menu.classList.toggle('show');
+    navMenu.classList.toggle('leleo');
 })
 
 contact.addEventListener('click', () => {
     menu.classList.toggle('show');
+    navMenu.classList.toggle('leleo');
 })
